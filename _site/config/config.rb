30314f59f7a552865c9d5add362c6f47208fc8f6
Evan Lori Mahone <apathinwalking@gmail.com>
2018-07-29 22:26:13 -0400

    deploy to server 1

---
+++ _site/config/config.rb
@@ -0,0 +1,5 @@
+set :application, 'evanlori.com'
+set :repo_url, 'git@github.com:apathinwalking/evanlori-dot-com.git'
+set :deploy_to, '/var/www/evanlori.com'
+set :ssh_options, { :forward_agent => true }
+set :repo_tree, '_site'
--- _site/config/deploy/production.rb
+++ _site/config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '198.199.90.179', user: 'deploy', roles: %w{web app}
+server '104.236.44.151', user: 'deploy', roles: %w{web app}
 
 # server-based syntax
 # ======================
