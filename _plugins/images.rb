a20337d228fbd388a145ad1e7416dd8ba708baa0
Evan Lori Mahone <apathinwalking@gmail.com>
2025-07-22 10:56:30 -0400

    new post

--- _plugins/images.rb
+++ _plugins/images.rb
@@ -1,25 +1,27 @@
 module Jekyll
   module ImageFilters
-    # def image_url_(public_id, type)
-    #   site = @context.registers[:site]
-    #   config = @context.registers[:site].config
-    #   data = @context.registers[:site].data
-    #   cloud_name = config['cloud_name']
-    #   all_ts = data['images']['transformations']
-    #   img_ts = all_ts.select { |item| item['public_id'] == public_id && item['type'] == type }.first
-    #   img_t_default = data['default']['transformation'][type]
-    #   all_formats = data['images']['formats']
-    #   img
-    def image_url(public_id, *transformation)
+    def image_url(public_id, type)
       site = @context.registers[:site]
-      cloud_name = site.config['cloud_name']
-      file_type="jpeg"
+      config = @context.registers[:site].config
+      data = @context.registers[:site].data
+      cloud_name = config['cloud_name']
+      
+      xforms = data['images']['transformations']
+      xform_d = data['images']['default']['transformation'][type]
+      xform_ovrd = xforms.select { |item| item['public_id'] == public_id && item['type'] == type }.first
+      xform = xform_ovrd && xform_ovrd['transformation'] || xform_d
+
+      formats = data['images']['formats']
+      format_d = data['images']['default']['format'][type]
+      format_ovrd = formats.select { |item| item['public_id'] == public_id && item['type'] == type }.first
+      format = format_ovrd && format_ovrd['format'] || format_d
+      
       url = "http://res.cloudinary.com/"
       url += "#{cloud_name}/"
       url += "image/"
       url += "upload/"
-      url += "#{transformation.join("/")}/" if transformation
-      url += "#{public_id}.#{file_type}"
+      url += "#{xform.join("/")}/" if xform
+      url += "#{public_id}.#{format}"
       url
     end
   end
